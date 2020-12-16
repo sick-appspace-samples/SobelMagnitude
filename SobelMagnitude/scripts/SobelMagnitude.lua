0c2889c0901f37fa946cd3279f695109d6d18ba8
rdzanmi <mikolaj.rdzanek@sick.de>
2020-12-16 12:17:05 +0100

    'Update'

--- SobelMagnitude/scripts/SobelMagnitude.lua
+++ SobelMagnitude/scripts/SobelMagnitude.lua
@@ -28,7 +28,7 @@ print('AppEngine Version: ' .. Engine.getVersion())
 local DELAY = 1000 -- ms between visualization steps for demonstration purpose
 
 -- Creating viewer
-local viewer = View.create()
+local viewer = View.create("viewer2D1")
 
 --End of Global Scope-----------------------------------------------------------
 
