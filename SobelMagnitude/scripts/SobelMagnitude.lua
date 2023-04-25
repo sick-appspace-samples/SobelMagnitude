d173388e637b200b02df5f56e99e6a7dbdec236a
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-04-25 18:28:55 +0000

    Update

--- SobelMagnitude/scripts/SobelMagnitude.lua
+++ SobelMagnitude/scripts/SobelMagnitude.lua
@@ -6,7 +6,7 @@ print('AppEngine Version: ' .. Engine.getVersion())
 local DELAY = 1000 -- ms between visualization steps for demonstration purpose
 
 -- Creating viewer
-local viewer = View.create("viewer2D1")
+local viewer = View.create()
 
 --End of Global Scope-----------------------------------------------------------
 
